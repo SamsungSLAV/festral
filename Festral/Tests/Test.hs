08d030e72576152be0dad4579d2a7f9403bf2393
Uladzislau Harbuz <u.harbuz@samsung.com>
2018-06-21 12:59:57 +0200

    More pretty logs, doesn't show rpm in output

--- Festral/Tests/Test.hs
+++ Festral/Tests/Test.hs
@@ -30,7 +30,7 @@ data TestResParser a = TCT TCTParser deriving Show
 
 instance TestParser (TestResParser a) where
     parse (TCT x) = parse x
-    
+
     fromWelesFiles x = do
         p <- fromWelesFiles x
         return $ TCT p
@@ -70,7 +70,7 @@ parseTest config outs buildDir outDir
 writeWithParser config outs buildDir outDir = do
     parser <- getParser (parser config) outs
     writeReportFile (parse parser) (outDir ++ "/report.txt")
-    
+
 writeWithOwn config outs buildDir outDir = do
     handle err $ do
         (inp, out, err, handle) <- runInteractiveProcess (parser config) [concat $ map (\(n,c) -> c) outs] Nothing Nothing
@@ -91,7 +91,7 @@ parseTest' writer config outs buildDir outDir = do
     let time = year ++ mounth ++ day ++ hour ++ min ++ secs
     tester <- getEffectiveUserName
     let testMeta = MetaTest meta tester tester time
-    
+
     let outDirName = outDir ++ "/" ++ hash meta ++ "_" ++ time
     catch (createDirectory outDirName) (recreate_dir outDirName 0)
     toFile testMeta (outDirName ++ "/meta.txt")
@@ -102,10 +102,10 @@ parseTest' writer config outs buildDir outDir = do
 
     where
         recreate_dir :: FilePath -> Int -> IOError -> IO ()
-        recreate_dir path i ex 
+        recreate_dir path i ex
             | isAlreadyExistsError ex = catch (createDirectory $ path ++ "_" ++ show i) (recreate_dir path (i+1))
             | otherwise = putStrLn $ show ex
-        
+
 
 getParser :: String -> [(String, String)] -> IO (TestResParser a)
 getParser _ testRes = do
@@ -120,11 +120,11 @@ buildCacheFileName = do
 -- runTest path_to_config_fiile build_name
 runTest :: TestRunnerConfig -> String -> IO (TestConfig, [(String, String)])
 runTest config target = do
-    metaStr <- readFile $ (buildLogDir config) ++ "/" ++  target ++ "/meta.txt"
+    metaStr <- readFile $ (buildLogDir config) ++ "/" ++ target ++ "/meta.txt"
     let meta = readMeta metaStr
     let yamlPath = getYaml $ getConf meta
 
-    putStrLn $ "Starting Weles job with " ++ yamlPath ++ " ..."
+    putStrLn $ "[" ++ repoName meta ++ "]Starting Weles job with " ++ yamlPath ++ " ..."
 
     let buildOutDir = (buildLogDir config) ++ "/" ++ target ++ "/build_res"
     rpms <- catch (getDirectoryContents buildOutDir) dirDoesntExists
@@ -132,26 +132,26 @@ runTest config target = do
     cachePath <- buildCacheFileName
     yamlCache <- catch (readFile cachePath) fileNotExists
     handle emptyFileNotExists $ writeFile (buildOutDir ++ "/test.yml") (generateFromTemplate yamlTemplate $ yamlTemplater rpms buildOutDir yamlCache)
-    jobId <- if status meta == "SUCCEED" && yamlTemplate /= "" 
+    jobId <- if status meta == "SUCCEED" && yamlTemplate /= ""
                 then handle badJob $ withCurrentDirectory buildOutDir $ startJob (buildOutDir ++ "/test.yml")
                 else return Nothing
 
-    putStrLn $ "Job id is " ++ show jobId
+    putStrLn $ "[" ++ repoName meta ++ "]Job id is " ++ show jobId
     let jobId' = if isNothing jobId
                         then return (-1)
                         else return $ fromJust jobId
     jobId'' <- jobId'
 
-    putStr "Waiting for job finished ... "
+    putStr $ "[" ++ repoName meta ++ "]Waiting for job finished ... "
     job <- getJobWhenDone jobId''
-    jobFiles <- getFileList jobId''
-    putStrLn $ "OK\n Recieved files: " ++ show jobFiles
+    jobFiles <- (filter (not . (isInfixOf ".rpm")) <$>) <$> getFileList jobId''
+    putStrLn $ "OK\n[" ++ repoName meta ++ "]Recieved files: " ++ show jobFiles
     let jobFiles' = if isNothing jobFiles
                         then []
                         else fromJust jobFiles
     let ret = mapM (\fname -> do
                     content <- getJobOutFile jobId'' fname
-                    let content' = if isNothing content 
+                    let content' = if isNothing content
                                     then ""
                                     else fromJust content
                     return (fname, content')
