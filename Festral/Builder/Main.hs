7c1efaccf1ae28e713d62356642eaf5a0b6709a6
Uladzislau Harbuz <u.harbuz@samsung.com>
2018-07-03 10:58:32 +0200

    Add new main to the festral-build, add it html generating

--- Festral/Builder/Main.hs
+++ Festral/Builder/Main.hs
@@ -8,23 +8,67 @@ import Data.Maybe
 import System.Directory
 import System.IO
 import Festral.Files
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Control.Monad
+import Festral.Builder.Reporter
+import Data.Time.LocalTime
 
-main = do
-    args <- getArgs
+main = runCmd =<< execParser options
+    where
+        options = info (opts <**> helper)
+            ( fullDesc
+            <>progDesc  "Build all repositories for all branches described in configuration file"
+            <>header    "Festral - simple client for tests management using Weles as test server")
 
+data Options = Options
+    { config    :: String
+    , reposPath :: String
+    , outDir    :: String
+    , htmlReport:: String
+    }
+
+opts :: Parser Options
+opts = Options
+    <$> strOption
+        (  long     "config"
+        <> metavar  "FILENAME"
+        <> short    'c'
+        <> help     "Configuration file" )
+    <*> strOption
+        (  long     "repos"
+        <> metavar  "DIRECTORY"
+        <> short    'r'
+        <> help     "Directory with repositories to build." )
+    <*> strOption
+        (  long     "out"
+        <> metavar  "DIRECTORY"
+        <> short    'o'
+        <> help     "Output root directory" )
+    <*> strOption
+        (  long     "html-out"
+        <> metavar  "DIRECTORY"
+        <> value    ""
+        <> help     "Output directory for summary report of the build. Generate report only if this option is specified, otherwise no report will be generated" )
+
+runCmd :: Options -> IO ()
+runCmd (Options config repos out report) = do
     freshBuildsFile <- freshBuilds
     writeFile freshBuildsFile ""
 
-    let res = if (length args) /= 3
-                then putStrLn "Usage: festral-build <config json> <repositoy location> <output directory>"
-                else do
-                    let [cfg, repodir, outdir] = args
-                    builder' <- builderFromFile cfg
-                    if isJust builder'
-                        then do
-                            let Just builder = builder'
-                            mapM_ (\x -> build x repodir outdir) builder
-                        else 
-                            putStrLn "Cant get builder from config file"
-    res
+    builder' <- builderFromFile config
+    if isJust builder'
+        then do
+            let Just builder = builder'
+            mapM_ (\x -> build x repos out) builder
+        else 
+            putStrLn "Cant get builder from config file"
+
+    when (report /= "") $ do
+        html <- reportHTML
+        time <- show <$> getZonedTime
+        writeFile (report ++ "/" ++ gentime time ++ ".html") html
+
+    where
+        gentime x = let (day:tm:_) = words x in day ++ "_" ++ tm
                                                                                                         
