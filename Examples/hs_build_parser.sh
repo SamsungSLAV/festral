2346639c66c02298c086e7debc6926682777bc67
Uladzislau Harbuz <u.harbuz@samsung.com>
2018-08-13 12:51:38 +0200

    Add example ow the own build parser

---
+++ Examples/hs_build_parser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BUILD_LOG="$1"
+STATUS="SUCCEED"
+
+while read -r line
+do
+    if [ "$line" = *"error"*  ]; then
+        STATUS="FAILED"
+    fi
+done < <($BUILD_LOG)
+TIMESTAMP=$(date +%Y%H%M%S)
+
+echo BOARD=x86_64
+echo BUILD_TYPE=debug
+echo COMMIT=unknown
+echo BUILD_TIME=$TIMESTAMP
+echo TOOLCHAIN=ghc
+echo BUILDER=unknown
+echo BUILD_STATUS=$STATUS
+echo BUILD_HASH=unknown
+echo OUT_DIR=$(pwd)/dist/build/
+echo REPO_NAME=unknown
+echo BRANCH=unknown
