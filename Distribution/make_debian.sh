db02dd7b2567dc73bd894a18bdad7f14f5120832
Uladzislau Harbuz <u.harbuz@samsung.com>
2019-01-21 15:48:09 +0100

    Add manpages. Move debian package creating into Distribution directory
    Change-Id: I8bbfb5563d0a4db9c8b91684a435fe8b330b3ef1

---
+++ Distribution/make_debian.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FESTRAL_BIN_PATH=$1
+FARMER_BIN_PATH=$2
+MANPAGES_OUT=$3
+MAN_INSTALL_PATH="/usr/local/man/man1"
+DEB_PATH="deb/festral"
+CONTROL="${DEB_PATH}/DEBIAN/control"
+POSTINST="${DEB_PATH}/DEBIAN/postinst"
+
+mkdir -p ${DEB_PATH}/usr/bin
+mkdir -p ${DEB_PATH}/DEBIAN
+mkdir -p ${DEB_PATH}/${MAN_INSTALL_PATH}
+cp ${FESTRAL_BIN_PATH}/festral deb/festral/usr/bin/festral
+cp ${FARMER_BIN_PATH}/farmer deb/festral/usr/bin/farmer
+
+#install manpages
+MANS=$(ls ${MANPAGES_OUT})
+for man in ${MANS}
+do
+    cat ${MANPAGES_OUT}/${man} | gzip > ${DEB_PATH}/${MAN_INSTALL_PATH}/${man}.1
+done
+
+echo "Package: festral" > ${CONTROL}
+echo "$(grep ^Version Festral.cabal)" >> ${CONTROL}
+echo "Maintainer: Uladzislau Harbuz" >> ${CONTROL}
+echo "Architecture: all" >> ${CONTROL}
+echo "Description: Automated testing system. Client for SLAV stack." >> ${CONTROL}
+echo "Depends: curl, git, ssh" >> ${CONTROL}
+
+echo "#!/bin/bash" > ${POSTINST}
+echo "festral --bash-completion-script festral >/etc/bash_completion.d/festral" >> ${POSTINST}
+echo "farmer --bash-completion-script farmer >/etc/bash_completion.d/farmer" >> ${POSTINST}
+chmod +x ${POSTINST}
+
+dpkg-deb --build deb/festral
